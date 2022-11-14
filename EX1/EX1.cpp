ef9041df62c50f6efccc8bf910cd86e8989d62c7
Andrey <89473462+Arty525@users.noreply.github.com>
2022-11-14 18:44:55 +0300

    Add files via upload

---
+++ EX1/EX1.cpp
@@ -0,0 +1,81 @@
+﻿//SUPERDOG
+#include <iostream>
+#include <vector>
+
+class Talent {
+protected:
+	std::string talent;
+public:
+	virtual std::string get_talent() = 0;
+};
+
+class Swimming : virtual public Talent {
+public:
+	virtual std::string  get_talent() {
+		return talent;
+	}
+
+	Swimming() {
+		talent = "swimming";
+	}
+};
+
+class Dancing : virtual public Talent {
+public:
+	virtual std::string  get_talent() {
+		return talent;
+	}
+
+	Dancing() {
+		talent = "dancing";
+	}
+};
+
+class Counting : virtual public Talent {
+public:
+	virtual std::string  get_talent() {
+		return talent;
+	}
+
+	Counting() {
+		talent = "counting";
+	}
+};
+
+class Dog {
+	std::string name;
+	std::vector<std::string> talents;
+public:
+	void showTalent() {
+		for (int i = 0; i < talents.size(); ++i) {
+			std::cout << "It can " << talents[i] << std::endl;
+		}
+	}
+
+
+	Dog(std::string inName){
+		name = inName;
+
+		Swimming swimming;
+		Dancing dancing;
+		Counting counting;
+
+		talents = { swimming.get_talent(), dancing.get_talent(), counting.get_talent() };
+	}
+};
+
+int main() {
+	std::string input;
+
+	std::cout << "Enter dog's name: ";
+	std::cin >> input;
+
+	Dog dog(input);
+
+	std::cout << "Command: ";
+	std::cin >> input;
+
+	if(input == "show_talent") dog.showTalent();
+
+	return 0;
+}
---
+++ EX2/EX2.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <cmath>
+
+class Shape {
+protected:
+	std::string figure;
+	double s = 0;
+	double bheight = 0;
+	double bwidth = 0;
+public:
+	virtual std::string type() = 0;
+	virtual double square() = 0;
+	virtual void border() = 0;
+	virtual void print(Shape* shape) {
+		shape->border();
+		std::cout << "Type: " << shape->type() << std::endl;
+		std::cout << "Square: " << shape->square() << std::endl;
+		std::cout << "Border width: " << shape->bwidth << " height: " << shape->bheight << std::endl;
+	}
+};
+
+class Triangle : virtual public Shape {
+	double a = 0;
+	double b = 0;
+	double c = 0;
+
+public:
+	virtual std::string type() {
+		return figure = "Triangle";
+	}
+
+	virtual double square() {
+		double p = (a + b + c) / 2;
+		s = sqrt(p * (p - a) * (p - b) * (p - c));
+		return s;
+	}
+
+	virtual void border() {
+		double p = (a + b + c) / 2;
+		bwidth = a* b* c / (4 * sqrt(p * (p - a) * (p - b) * (p - c))) * 2;
+		bheight = bwidth;
+	}
+
+	Triangle(double inA, double inB, double inC) {
+		a = inA;
+		b = inB;
+		c = inC;
+	}
+};
+
+class Circle : virtual public Shape {
+	double radius = 0;
+
+public:
+	virtual std::string type() {
+		return figure = "Circle";
+	}
+
+	virtual double square() {
+		s = atan(1) * 4 * radius;
+		return s;
+	}
+
+	virtual void border() {
+		bwidth = radius * 2;
+		bheight = bwidth;
+	}
+
+	Circle(double inRadius) {
+		radius = inRadius;
+	}
+};
+
+class Rectangle : virtual public Shape {
+	double a = 0;
+	double b = 0;
+
+public:
+	virtual std::string type() {
+		return figure = "Rectangle";
+	}
+
+	virtual double square() {
+		s = a * b;
+		return s;
+	}
+
+	virtual void border() {
+		bwidth = a;
+		bheight = b;
+	}
+
+	Rectangle(double inA, double inB) {
+		a = inA;
+		b = inB;
+	}
+};
+
+int main() {
+	Triangle t(3, 4, 5);
+	t.print(&t);
+	return 0;
+}
